cdbe48fd5537afa455a8ce7849009327f0cfc345
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-10 15:49:53 +0530

    contacts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
-import { Container, Typography, TextField, Button } from '@mui/material';
+import React from "react";
+import { Container, TextField, Button, Typography, Stack } from "@mui/material";
 
-const Contact = () => (
-  <Container>
-    <Typography variant="h4" gutterBottom>
-      Contact Me
-    </Typography>
-    <TextField fullWidth label="Name" margin="normal" />
-    <TextField fullWidth label="Email" margin="normal" />
-    <TextField fullWidth label="Message" margin="normal" multiline rows={4} />
-    <Button variant="contained" color="primary">
-      Submit
-    </Button>
-  </Container>
-);
+const Contact = () => {
+  return (
+    <Container maxWidth="sm" style={{ marginTop: "2rem" }}>
+      <Typography variant="h4" gutterBottom>
+        FormSubmit Demo
+      </Typography>
+      <form
+        action="https://formsubmit.co/f0fa9848164c196456b945cae75bbd82"
+        method="POST"
+        target="_blank"
+      >
+        <Stack spacing={2}>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              fullWidth
+              name="name"
+              label="Full Name"
+              variant="outlined"
+              required
+            />
+            <TextField
+              fullWidth
+              name="email"
+              type="email"
+              label="Email Address"
+              variant="outlined"
+              required
+            />
+          </Stack>
+          <TextField
+            fullWidth
+            name="message"
+            label="Your Message"
+            variant="outlined"
+            multiline
+            rows={10}
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+          >
+            Submit Form
+          </Button>
+        </Stack>
+      </form>
+    </Container>
+  );
+};
 
 export default Contact;
